024e178f30d714fe939c36b5d93ba0413dc5fb51
Gerald <ngjingsiang@gmail.com>
2017-09-01 18:25:19 +0800

    Add in stub posts

--- Spinda/ViewController.swift
+++ Spinda/ViewController.swift
@@ -13,12 +13,20 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var postTableView: UITableView!
 
-    let topics = ["1", "123123", "g0bq9fCKfhYbeAAIzXubvZnemCgsn6Xjs3tPiVtmp8nDTLgC3vZzVnkiMCUVOMSDf4raqghyClrbuSAyLDCZbVK6b2c61TPX3l0tevNeIKIyNiKhJZl3SB5tOveas3mhiIvczuvjrdPgV3NJRKzr6My1HyEmrdbM8sOJO6w13pwJMzwdPJWatBDj76MlxILWWptzTN3pKA1LnDeJUBTJFE3xE0nruhUHILoPIlaYm4cXyIq2O2jEjcgqPojL7wy", "g0bq9fCKfhYbeAAIzX"]
+    var posts = [Post]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         postTableView.register(UINib(nibName: "PostTableViewCell", bundle: nil), forCellReuseIdentifier: cellIdentifier)
+        let post1 = Post(topic: "1", upvote: 0, downvote: 0)
+        let post2 = Post(topic: "topic 2 g0bq9fCKfhYbeAAIzXubvZnemCgsn6Xjs3tPiVtmp8nDTLgC3vZzVnkiMCUVOMSDf4raqghyClrbuSAyLDCZbVK6b2c61TPX3l0tevNeIKIyNiKhJZl3SB5tOveas3mhiIvczuvjrdPgV3NJRKzr6My1HyEmrdbM8sOJO6w13pwJMzwdPJWatBDj76MlxILWWptzTN3pKA1LnDeJUBTJFE3xE0nruhUHILoPIlaYm4cXyIq2O2jEjcgqPojL7wy", upvote: 0, downvote: 0)
+        let post3 = Post(topic: "topic 3 g0bq9fCKfhYb", upvote: 0, downvote: 0)
+        let post4 = Post(topic: "topic 4 g0bq9fCKfhYbg0bq9fCKfhYbg0bq9fCKfhYbg0bq9fCKfhYbg0bq9fCKfhYb", upvote: 0, downvote: 0)
 
+        posts.append(post1)
+        posts.append(post2)
+        posts.append(post3)
+        posts.append(post4)
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,12 +36,12 @@ class ViewController: UIViewController {
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return topics.count
+        return posts.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! PostTableViewCell
-        cell.topicLabel.text = topics[indexPath.row]
+        cell.topicLabel.text = posts[indexPath.row].topic
 
         return cell
     }
