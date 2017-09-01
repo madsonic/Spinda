8d24a9493f710904deeb1f511a527fb94a0d953b
Gerald <ngjingsiang@gmail.com>
2017-09-01 18:02:14 +0800

    Hook up basic custom cell for displaying topic

---
+++ Spinda/PostTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  PostTableViewCell.swift
+//  Spinda
+//
+//  Created by Gerald on 1/9/17.
+//  Copyright © 2017 com.gerald. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+    @IBOutlet weak var topicLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- Spinda/ViewController.swift
+++ Spinda/ViewController.swift
@@ -11,8 +11,14 @@ import UIKit
 class ViewController: UIViewController {
     let cellIdentifier = "topicCellIdentifier"
 
+    @IBOutlet weak var postTableView: UITableView!
+
+    let topics = ["1", "123123", "g0bq9fCKfhYbeAAIzXubvZnemCgsn6Xjs3tPiVtmp8nDTLgC3vZzVnkiMCUVOMSDf4raqghyClrbuSAyLDCZbVK6b2c61TPX3l0tevNeIKIyNiKhJZl3SB5tOveas3mhiIvczuvjrdPgV3NJRKzr6My1HyEmrdbM8sOJO6w13pwJMzwdPJWatBDj76MlxILWWptzTN3pKA1LnDeJUBTJFE3xE0nruhUHILoPIlaYm4cXyIq2O2jEjcgqPojL7wy", "g0bq9fCKfhYbeAAIzX"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        postTableView.register(UINib(nibName: "PostTableViewCell", bundle: nil), forCellReuseIdentifier: cellIdentifier)
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,11 +28,14 @@ class ViewController: UIViewController {
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20
+        return topics.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! PostTableViewCell
+        cell.topicLabel.text = topics[indexPath.row]
+
+        return cell
     }
 }
 
