fc46fe270b94d89c10941817af2c6577ced74282
Gerald <ngjingsiang@gmail.com>
2017-09-01 16:52:12 +0800

    Add Post struct for storing post's data

---
+++ Spinda/Post.swift
@@ -0,0 +1,13 @@
+//
+//  Post.swift
+//  Spinda
+//
+//  Created by Gerald on 1/9/17.
+//  Copyright © 2017 com.gerald. All rights reserved.
+//
+
+struct Post {
+    let topic: String
+    var upvote: Int
+    var downvote: Int
+}
