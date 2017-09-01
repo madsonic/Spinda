5591ea187c5571a1140b895fcaeb738f2fa9ab34
Gerald <ngjingsiang@gmail.com>
2017-09-01 16:56:27 +0800

    Add basic table view and cell

--- Spinda/ViewController.swift
+++ Spinda/ViewController.swift
@@ -9,17 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let cellIdentifier = "topicCellIdentifier"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+
+}
