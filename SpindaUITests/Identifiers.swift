4959efe18bc33ef5ff92663b5bd6657a991915e9
Gerald <ngjingsiang@gmail.com>
2017-09-03 16:48:50 +0800

    Add identifiers as enum

---
+++ SpindaUITests/Identifiers.swift
@@ -0,0 +1,17 @@
+//
+//  Identifiers.swift
+//  SpindaUITests
+//
+//  Created by Gerald on 3/9/17.
+//  Copyright © 2017 com.gerald. All rights reserved.
+//
+
+enum Identifier: String {
+    case createPostButtonIdentifer = "createPostButton",
+    createPostModalIdentifer = "createPostModal",
+    topicViewIdentifer = "topicView",
+    confirmCreatePostButtonIdentifer = "confirmCreatePostButton",
+    cancelCreatePostButtonIdentifer = "cancelCreatePostButton",
+    upvoteButtonIdentifier = "upvoteButton",
+    downvoteButtonIdentifier = "downvoteButton"
+}
